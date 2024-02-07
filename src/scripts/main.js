f17135ed84bec7c8384019f03951dbdf6d2b3018
Rodrigo Sumioshi <shodirodrigo@gmail.com>
2024-02-07 11:10:10 -0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+// Arquivo de exemplo para minificação
+
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+console.log(soma(5, 3)); // Saída esperada: 8
+console.log(subtracao(10, 4)); // Saída esperada: 6
